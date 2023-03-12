33449c9d9a51a375e04d72d3c73b3732dad9dac9
Sergey Kirichenko <spkirich@gmail.com>
2023-03-12 18:12:24 +0300

    Add flake apps

--- flake.nix
+++ flake.nix
@@ -9,35 +9,49 @@
 
     in
     {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "bmstu-06-os-02";
+      apps.${system} = {
+        client = {
+          program = "${self.packages.${system}.default}/bin/client";
+          type = "app";
+        };
 
-        buildInputs = with pkgs; [
-          gcc
-        ];
-
-        buildPhase = ''
-          gcc client.c -o client
-          gcc server.c -o server
-        '';
-
-        installPhase = ''
-          mkdir -p $out/bin
-
-          mv client $out/bin
-          mv server $out/bin
-        '';
-
-        src = ./src;
+        server = {
+          program = "${self.packages.${system}.default}/bin/server";
+          type = "app";
+        };
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          self.packages.${system}.default
-          man-pages
-        ];
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            self.packages.${system}.default
+            man-pages
+          ];
+        };
       };
 
       formatter.${system} = pkgs.nixpkgs-fmt;
+
+      packages.${system} = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "bmstu-06-os-02";
+
+          buildInputs = with pkgs; [
+            gcc
+          ];
+
+          buildPhase = ''
+            gcc client.c -o client
+            gcc server.c -o server
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            mv client server $out/bin
+          '';
+
+          src = ./src;
+        };
+      };
     };
 }
