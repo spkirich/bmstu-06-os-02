024fd705a91066c129c67db00518cb9fb1718a1b
Sergey Kirichenko <spkirich@gmail.com>
2023-03-13 12:41:42 +0300

    Get rid of threads

--- src/server.c
+++ src/server.c
@@ -1,52 +1,18 @@
 #include <arpa/inet.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define CMAX 8
 #define PORT 8484
-
-void *listen_thread(void *args)
-{
-  int epfd = ((int *) args)[0];
-  int sock = ((int *) args)[1];
-
-  while (1)
-  {
-    if (listen(sock, 8) == -1)
-    {
-      perror("Failed to listen on the socket");
-      exit(1);
-    }
-
-    int conn;
-
-    if ((conn = accept(sock, NULL, NULL)) == -1)
-    {
-      perror("Failed to accept a connection");
-      exit(1);
-    }
-
-    struct epoll_event epev;
-
-    epev.events = EPOLLIN;
-    epev.data.fd = conn;
-
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, conn, &epev) == -1)
-    {
-      perror("Failed to add the connection to the event poll");
-      exit(1);
-    }
-  }
-}
 
 int main()
 {
   int epfd;
 
-  if ((epfd = epoll_create(8)) == -1)
+  if ((epfd = epoll_create(CMAX)) == -1)
   {
     perror("Failed to create an event poll");
     exit(1);
@@ -80,55 +46,83 @@ int main()
     exit(1);
   }
 
-  int args[2];
-
-  args[0] = epfd;
-  args[1] = sock;
-
-  pthread_t thread;
-
-  if (pthread_create(&thread, NULL, listen_thread, args) != 0)
+  if (listen(sock, CMAX) == -1)
   {
-    perror("Failed to create a thread");
+    perror("Failed to listen on the socket");
     exit(1);
   }
 
   struct epoll_event epev;
 
+  epev.events = EPOLLIN;
+  epev.data.fd = sock;
+
+  if (epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &epev) == -1)
+  {
+    perror("Failed to add the socket to the event poll");
+    exit(1);
+  }
+
   while (1)
   {
+    struct epoll_event epev[CMAX + 1];
     int nfds;
 
-    if ((nfds = epoll_wait(epfd, &epev, 1, 0)) == -1)
+    if ((nfds = epoll_wait(epfd, &epev, CMAX + 1, -1)) == -1)
     {
       perror("Failed to wait for an event");
       exit(1);
     }
 
-    if (nfds > 0)
+    for (int i = 0; i < nfds; i++)
     {
-      int conn = epev.data.fd;
-
-      const size_t len = 1024;
-      char req[len], res[len];
-
-      if (recv(conn, req, len, 0) == -1)
+      if (epev[i].data.fd == sock)
       {
-        perror("Failed to receive a message");
-        exit(1);
+        int conn;
+
+        if ((conn = accept(sock, NULL, NULL)) == -1)
+        {
+          perror("Failed to accept a connection");
+          exit(1);
+        }
+
+        struct epoll_event epev;
+
+        epev.events = EPOLLIN;
+        epev.data.fd = conn;
+
+        if (epoll_ctl(epfd, EPOLL_CTL_ADD, conn, &epev) == -1)
+        {
+          perror("Failed to add the connection to the event poll");
+          exit(1);
+        }
       }
 
-      printf("Server %d got: %s\n", getpid(), req);
-      sprintf(res, "%d+%s", getpid(), req);
-
-      if (send(conn, res, sizeof(res), 0) == -1)
+      else
       {
-        perror("Failed to send a message");
-        exit(1);
-      }
+        int conn = epev[i].data.fd;
 
-      printf("Server %d put: %s\n", getpid(), res);
-      close(conn);
+        const size_t len = 1024;
+        char req[len], res[len];
+
+        if (recv(conn, req, len, 0) == -1)
+        {
+          perror("Failed to receive a message");
+          exit(1);
+        }
+
+        printf("Server %d got: %s\n", getpid(), req);
+        sprintf(res, "%d+%s", getpid(), req);
+
+        if (send(conn, res, sizeof(res), 0) == -1)
+        {
+          perror("Failed to send a message");
+          exit(1);
+        }
+
+        printf("Server %d put: %s\n", getpid(), res);
+        close(conn);
+      }
     }
   }
 
